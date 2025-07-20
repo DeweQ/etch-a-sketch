e4fb52e334a5dde88720579a10cc5249a87dee71
DeweQ <thedeweq@yandex.ru>
2025-07-20 16:52:34 +0300

    Add controls to change size and clear grid

--- script.js
+++ script.js
@@ -5,15 +5,34 @@ grid.addEventListener("mouseover", e => {
   if (e.target.classList.contains("cell"))
     draw(e.target,"black");
 });
+let gridSize = 16;
 let flexSheet = document.querySelector("#flex-sheet");
+const changeGridSizeButton = document.querySelector("#change-size");
+changeGridSizeButton.addEventListener("click", changeGridSize);
+const clearButton = document.querySelector("#clear-grid");
+clearButton.addEventListener("click", ()=> generateGrid(gridSize));
+function changeGridSize() {
+  let result = prompt("Enter desired size: ");
+  let size = parseInt(result);
+  if (isNaN(size)) {
+    alert(`"${result}" is not a number.`);
+    return;
+  }
+  if (2 > size || size > 100) {
+    alert("Out of range. Please enter a number between 2 and 100");
+    return;
+  }
+  generateGrid(size);
+}
 function generateGrid(size) {
-  grid.innerHtml = "";
+  grid.innerHTML = "";
   for (let i = 0; i < size * size; i++) {
     const cell = document.createElement("div");
     cell.classList.add("cell", "adjust-flex-basis");
     grid.appendChild(cell);
   }
   updateFlexSheet(size);
+  gridSize = size;
 }
 function updateFlexSheet(size) {
   if (flexSheet)
@@ -27,7 +46,7 @@ function updateFlexSheet(size) {
 function draw(target,color) {
   target.style.backgroundColor = color;
 }
-generateGrid(16);
+generateGrid(gridSize);
 
 } catch(e) {alert(e); }
 
