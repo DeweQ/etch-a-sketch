787f147eae520e200b47b519569e009ff900ee5c
DeweQ <thedeweq@yandex.ru>
2025-07-20 07:29:16 +0300

    Allign grid of squares in container

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-const grid = document.querySelector("#grid");
+try {
+
+const grid = document.querySelector("#container");
+let flexSheet = document.querySelector("#flex-sheet");
 let gridSize = 16;
-for (let i = 0; i < gridSize; i++) {
+for (let i = 0; i < gridSize * gridSize; i++) {
     const cell = document.createElement("div");
-    cell.classList.add("cell");
+    cell.classList.add("cell", "adjust-flex-basis");
     grid.appendChild(cell);
-} 
+}
+
+if (flexSheet)
+   document.head.removeChild(flexSheet);
+
+flexSheet = document.createElement("style");
+
+flexSheet.id = "flex-sheet";
+document.head.appendChild(flexSheet);
+flexSheet.sheet.insertRule(`.adjust-flex-basis { flex: 0 0 calc(100% / ${gridSize});}`);
+} catch(e) {alert(e); }
+
+
+
