00cac5f09f782c9d3f23f4cb8601dd7537ae090f
DeweQ <thedeweq@yandex.ru>
2025-07-20 10:07:54 +0300

    Move grid generetion to distinct function

--- script.js
+++ script.js
@@ -2,21 +2,25 @@ try {
 
 const grid = document.querySelector("#container");
 let flexSheet = document.querySelector("#flex-sheet");
-let gridSize = 16;
-for (let i = 0; i < gridSize * gridSize; i++) {
+
+function generateGrid(size) {
+  grid.innerHtml = "";
+  for (let i = 0; i < size * size; i++) {
     const cell = document.createElement("div");
     cell.classList.add("cell", "adjust-flex-basis");
     grid.appendChild(cell);
+  }
+  updateFlexSheet(size);
 }
-
-if (flexSheet)
-   document.head.removeChild(flexSheet);
-
-flexSheet = document.createElement("style");
-
-flexSheet.id = "flex-sheet";
-document.head.appendChild(flexSheet);
-flexSheet.sheet.insertRule(`.adjust-flex-basis { flex: 0 0 calc(100% / ${gridSize});}`);
+function updateFlexSheet(size) {
+  if (flexSheet)
+    document.head.removeChild(flexSheet);
+    flexSheet = document.createElement("style");
+    flexSheet.id = "flex-sheet";
+    document.head.appendChild(flexSheet);
+    flexSheet.sheet.insertRule(`.adjust-flex-basis { flex: 0 0 calc(100% / ${size});}`);
+}
+generateGrid(8);
 } catch(e) {alert(e); }
 
 
